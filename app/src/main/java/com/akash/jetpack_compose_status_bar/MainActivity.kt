55fc36c91a7f9ec88e55f69960177857627ed819
Akashpal12 <akashpalvibrant@gmail.com>
2025-11-22 12:03:39 +0530

    Initial commit

---
+++ app/src/main/java/com/akash/jetpack_compose_status_bar/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.akash.jetpack_compose_status_bar
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.akash.jetpack_compose_status_bar.ui.theme.Jetpack_compose_status_barTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Jetpack_compose_status_barTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Jetpack_compose_status_barTheme {
+        Greeting("Android")
+    }
+}
